466094e95383832f56c1bb0af5ddfddfae3e3b7e
snehachaudhary0804 <snehachaudhary0804@gmail.com>
2026-06-24 01:54:26 +0530

    Setup MongoDB connection and user model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose=require ("mongoose");
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["student","teacher","admin"],
+        default:"student"
+    }
+},
+{
+    timestamp:true
+}
+);
+module.exports=mongoose.model("User",userSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors= require("cors");
+const dotenv =require("dotenv");
+const connectDB=require("./config/db")
 
+
+dotenv.config();   
+connectDB();
 const app=express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("CISM Backend Running");
 });
-app.listen(5000,()=>{
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
     console.log("Server running on port 5000");
 });
