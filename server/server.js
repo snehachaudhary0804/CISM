cd145144bacbf39a5f88a642b63fc4b9642bc5c2
snehachaudhary0804 <snehachaudhary0804@gmail.com>
2026-06-29 01:40:36 +0530

    Add user authentication with bcrypt password hashing and JWT login

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.use("/api/auth", authRoutes);
 app.get("/",(req,res)=>{
     res.send("CISM Backend Running");
 });
+
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log("Server running on port 5000");
